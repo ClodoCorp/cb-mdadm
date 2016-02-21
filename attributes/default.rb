78e2a467385b39c8da86e01d4d0245dd0f39772f
Vasiliy Tolstov <v.tolstov@selfip.ru>
2016-02-21 16:32:58 +0000

    add alt
    Signed-off-by: Vasiliy Tolstov <v.tolstov@selfip.ru>

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 default['mdadm']['packages'] = value_for_platform(
-  %w(ubuntu debian exherbo archlinux centos redhat suse fedora) => {
+  %w(alt ubuntu debian exherbo archlinux centos redhat suse fedora) => {
     'default' => ['mdadm']
   }
 )
